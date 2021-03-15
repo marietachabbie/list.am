2ed1a87d5ca8068675cac5341a6c25bbf1971e6a
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2021-03-15 05:04:17 +0400

    feature/announcements-in-a-grid

--- frontend/src/components/Announcements.vue
+++ frontend/src/components/Announcements.vue
@@ -1,38 +1,23 @@
 <template>
     <div class="container">
-        <sui-segment id="announcements">
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
-            <h3>announcements</h3>
+        <sui-segment class="ui grid" id="announcements">
+            <div class="row">
+                <div class="five wide column">
+                    <a href="#">
+                        <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" />
+                    hello</a>
+                </div>
+                <div class="five wide column">
+                    <a href="#">
+                        <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" />
+                    hello</a>
+                </div>
+                <div class="five wide column">
+                    <a href="#">
+                        <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" />
+                    hello</a>
+                </div>
+            </div>
         </sui-segment>
     </div>
 </template>
@@ -60,6 +45,10 @@ export default {
     }
     #announcements {
         margin-top: 10px;
+        border-radius: 10px;
+    }
+    img {
+        width: 200px;
         border-radius: 10px;
     }
 </style>
