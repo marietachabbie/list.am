f804b36ecc85698772b05e8a25c63eb597e61efc
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2021-03-26 20:48:08 +0400

    feature/error handling on some parts

--- app/routes/antiquities.js
+++ app/routes/antiquities.js
@@ -4,14 +4,16 @@ const util = require('../data/util');
 const loadAnnouncements = util.loadAnnouncements;
 const loadSingleAnnouncement = util.loadSingleAnnouncement;
 
-router.get('/', async (req, res) => {
-    const announcements = await loadAnnouncements();
-    res.send(announcements);
+router.get('/', async (req, res, next) => {
+    return loadAnnouncements()
+    .then(announcements => res.send(announcements))
+    .catch(next);
 })
 
-router.get('/:id', async (req, res) => {
-    const specific = await loadSingleAnnouncement(req.params.id);
-    res.send(specific);
+router.get('/:id', async (req, res, next) => {
+    return loadSingleAnnouncement(req.params.id)
+    .then(specific => res.send(specific))
+    .catch(next);
 })
 
 module.exports = router;
