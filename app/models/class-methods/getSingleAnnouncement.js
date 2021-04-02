fc6916ce7028e8255eff192ad1fd4b431a00084a
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2021-04-02 18:44:39 +0400

    feature/connect once when the app is ran-single announcements

--- app/models/class-methods/getSingleAnnouncement.js
+++ app/models/class-methods/getSingleAnnouncement.js
@@ -1,20 +1,11 @@
 const output = require('../../data/util').output;
+const mongodb = require('mongodb');
+const MongoDbConnection = require('../MongoDbConnection');
 
 const getSingleAnnouncements = (id) => {
     const objectID = new mongodb.ObjectID(id);
-    const announcements = CollectionAnnouncements.init()
-    .then(() => { return announcements.findOne({'_id': objectID}) })
+    return MongoDbConnection.announcementsCollection.find({'_id': objectID}).toArray()
     .catch((error) => output(error));
 }
 
 module.exports = getSingleAnnouncements;
-
-// loadSingleAnnouncement: async (id) => {
-//     try {
-//         const objectID = new mongodb.ObjectID(id);
-//         const announcements = await collection();
-//         return announcements.findOne({'_id': objectID});
-//     } catch (error) {
-//         output(error);
-//     }
-// },
